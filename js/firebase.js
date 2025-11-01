7bb5d7806edf9b3654504004b46ca3bc2373e56e
kbcglugur <kbcgl.ugur@gmail.com>
2025-11-01 13:48:57 +0300

    Update Firebase configuration with new credentials

--- js/firebase.js
+++ js/firebase.js
@@ -1,23 +1,24 @@
 // js/firebase.js
 
-// Firebase SDK'larından gerekli fonksiyonları içe aktar
-// Not: SDK'lar HTML'den global olarak (compat) yüklendiği için
-// 'firebase/app' yerine global 'firebase' nesnesini kullanıyoruz.
-
-// SİZİN 't-plans' PROJENİZİN YAPILANDIRMASI
+// SİZİN YAPIŞTIRDIĞINIZ EN GÜNCEL BİLGİLER:
+// (Bu blok, sizin sağladığınız en güncel config bilgisidir)
 const firebaseConfig = {
-  apiKey: "AIzaSyCBavvfozRwX8agNU9sT12boAU9A2J-6UI",
+  apiKey: "AIzaSyCwA2FfBavvfozJDaghRk12bcQkU9aZv5U",
   authDomain: "t-plans.firebaseapp.com",
   projectId: "t-plans",
   storageBucket: "t-plans.firebasestorage.app",
   messagingSenderId: "202431957522",
-  appId: "1:202431957522:web:6922a7bca6e5a51791a6cb",
-  measurementId: "G-D1YX50K6H9"
+  appId: "1:202431957522:web:6932a7bca6e5e51791a6cb",
+  measurementId: "G-D11X5QKSH9"
 };
+
+
+// BİZİM UYGULAMAMIZIN KULLANDIĞI YAPI:
+// (Biz 'import' değil, index.html'deki global 'firebase' nesnesini kullanıyoruz)
 
 // Firebase'i başlat
 const app = firebase.initializeApp(firebaseConfig);
-
+    
 // Kullanılacak servisleri al ve ihraç et
 export const auth = firebase.auth();
 export const db = firebase.firestore();
@@ -25,3 +26,4 @@ export const storage = firebase.storage();
 
 // Google Auth Sağlayıcısını ihraç et
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
+
